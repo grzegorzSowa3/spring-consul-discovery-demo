a30658d9c4a3af721519812551002249b8d1a16c
Grzegorz Sowa <grzegorz.sowa314@gmail.com>
2022-04-04 21:13:11 +0200

    update helm provider data

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,8 +20,8 @@ provider "google" {
 provider "helm" {
   kubernetes {
     token                  = data.google_client_config.default.access_token
-    host                   = data.google_container_cluster.vpc_native_cluster.endpoint
-    cluster_ca_certificate = base64decode(data.google_container_cluster.vpc_native_cluster.master_auth[0].cluster_ca_certificate)
+    host                   = resource.google_container_cluster.vpc_native_cluster.endpoint
+    cluster_ca_certificate = base64decode(resource.google_container_cluster.vpc_native_cluster.master_auth[0].cluster_ca_certificate)
   }
 }
 
